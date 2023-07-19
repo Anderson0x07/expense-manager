c03df4f64b4cf59f0b6733f8ee4e62e91b4e6ab1
Anderson0x07 <andersonadrianorro@ufps.edu.co>
2023-07-19 17:12:15 -0500

    The balance calculation is performed

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -1,12 +1,16 @@
 import { useGlobalState } from "../context/GlobalState";
 
 const Balance = () => {
+  const { transactions } = useGlobalState();
 
-    const data = useGlobalState()
+  const amount = transactions.map((item) => item.amount);
+
+  const total = amount.reduce((acc, item) => (acc += item), 0);
 
   return (
     <div>
-      <h1>Balance</h1>
+      <h3>Your Balance</h3>
+      <h1>$ {total.toLocaleString()}</h1>
     </div>
   );
 };
