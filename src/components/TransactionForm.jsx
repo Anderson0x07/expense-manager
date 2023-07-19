b8ec01d260b508ca2c25a4e447ff6c26995c1280
Anderson0x07 <andersonadrianorro@ufps.edu.co>
2023-07-19 16:19:56 -0500

    Creation of initial components and context

---
+++ src/components/TransactionForm.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+import { useGlobalState } from "../context/GlobalState"
+
+const TransactionForm = () => {
+
+    const { addTransaction } = useGlobalState();
+
+    const [description, setDescription] = useState('')
+    const [amount, setAmount] = useState(0)
+    
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        addTransaction()
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Enter a description" 
+                    onChange={(e) => setDescription(e.target.value)}/>
+                <input type="number" placeholder="0.0" step='0.01'
+                    onChange={(e) => setAmount(e.target.value)}/>
+                <button>Add transaction</button>
+            </form>
+        </div>
+    )
+}
+
+export default TransactionForm
---
+++ src/context/AppReducer.jsx
@@ -0,0 +1,11 @@
+
+const AppReducer = (state, action) => {
+  switch(action.type) {
+    case 'ADD_TRANSACTION':
+      return state
+    default:
+      return state
+  }
+}
+
+export default AppReducer
